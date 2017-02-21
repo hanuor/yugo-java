3082a93f6654dba910b48880969082ada93ccfba
hanuor <hanuortech@gmail.com>
2017-02-21 12:29:54 +0530

    First commit

---
+++ src/Driver.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Shantanu Johri on 21-02-2017.
+ */
+public class Driver {
+
+public static void main(String args[]){
+
+}
+}
---
+++ src/Perceptron.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Shantanu Johri on 21-02-2017.
+ */
+public class Perceptron {
+    public static final int[][][] data = {{{0, 0}, {0}}, {{0, 1}, {0}}, {{1, 0}, {0}}, {{1, 1}, {1}}};
+    public static final double LEARNING_RATE = 0.05;
+    public static final double[] INITIAL_WEIGHTS = {Math.random(), Math.random()};
+
+
+    public double calculatedWeightedSum(int[] data, double[] weights) {
+        return 0.0;
+    }
+}

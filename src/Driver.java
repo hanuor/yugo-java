19f74bc9d36c132bbd3af1569b96af0cb6491ef7
hanuor <hanuortech@gmail.com>
2017-02-21 12:56:57 +0530

    Push Driver

--- src/Driver.java
+++ src/Driver.java
@@ -8,6 +8,14 @@ public static void main(String args[]){
     double[] weights = Perceptron.INITIAL_WEIGHTS;
     Driver driver = new Driver();
     Perceptron perceptron = new Perceptron();
-
+    boolean errorFlag = true;
+    double error = 0.0;
+    while(errorFlag){
+        for(int i = 0; i< data.length; i++){
+            double weightedSum = perceptron.calculatedWeightedSum(data[i][0], weights);
+            int result = perceptron.applyActivationFunction(weightedSum);
+            error = data[i][1][0] - result;
+        }
+    }
 }
 }

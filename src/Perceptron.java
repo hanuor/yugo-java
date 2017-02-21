b3e7923d7b8a45eaed8c4f961c985e52a0e19835
hanuor <hanuortech@gmail.com>
2017-02-21 13:31:56 +0530

    ANN Complete

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -2,7 +2,7 @@
  * Created by Shantanu Johri on 21-02-2017.
  */
 public class Perceptron {
-    public static final int[][][] data = {{{0, 0}, {0}}, {{0, 1}, {0}}, {{1, 0}, {0}}, {{1, 1}, {1}}};
+    public static final int[][][] data = {{{0, 0}, {0}}, {{0, 1}, {0}}, {{1, 0}, {0}}, {{1, 1}, {1}},{{1, 1}, {1}}};
     public static final double LEARNING_RATE = 0.05;
     public static final double[] INITIAL_WEIGHTS = {Math.random(), Math.random()};
 
